6b493cbffef37f78dbd258ca2f48e5d9a7d0098d
Oskar A. Padu <oapadu@tlu.ee>
2025-05-15 11:06:36 +0000

    liides ja muu värk

---
+++ 06_liides/weapons.js
@@ -0,0 +1,42 @@
+var Sword = /** @class */ (function () {
+    function Sword(name, damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+    Sword.prototype.use = function () {
+        console.log("".concat(this.name, " l\u00F5ikab vaenlast! Kahju: ").concat(this.damage));
+    };
+    return Sword;
+}());
+var LaserGun = /** @class */ (function () {
+    function LaserGun(name, damage, range) {
+        this.name = name;
+        this.damage = damage;
+        this.range = range;
+    }
+    LaserGun.prototype.use = function () {
+        console.log("".concat(this.name, " tulistab laserkiirega! Kahju: ").concat(this.damage, ", Ulatus: ").concat(this.range, "m"));
+    };
+    return LaserGun;
+}());
+var Pistol = /** @class */ (function () {
+    function Pistol(name, damage, accuracy) {
+        this.name = name;
+        this.damage = damage;
+        this.accuracy = accuracy;
+    }
+    Pistol.prototype.use = function () {
+        console.log("".concat(this.name, " laseb! Kahju: ").concat(this.damage, ", T\u00E4psus: ").concat(this.accuracy, "%"));
+    };
+    return Pistol;
+}());
+var inventory = [
+    new Sword("KV Multitool", 25),
+    new LaserGun("Punane Laser", 40, 300),
+    new Pistol("Glock", 15, 150),
+];
+console.log("Mängija relvad:");
+inventory.forEach(function (weapon) {
+    console.log("- ".concat(weapon.name, " ").concat(weapon.damage, " "));
+    weapon.use();
+});
---
+++ 06_liides/weapons.ts
@@ -0,0 +1,63 @@
+interface Weapon {
+  name: string;
+  damage: number;
+  use(): void;
+}
+
+class Sword implements Weapon {
+  name: string;
+  damage: number;
+
+  constructor(name: string, damage: number) {
+    this.name = name;
+    this.damage = damage;
+  }
+
+  use(): void {
+    console.log(`${this.name} lõikab vaenlast! Kahju: ${this.damage}`);
+  }
+}
+
+class LaserGun implements Weapon {
+  name: string;
+  damage: number;
+  range: number;
+
+  constructor(name: string, damage: number, range: number) {
+    this.name = name;
+    this.damage = damage;
+    this.range = range;
+  }
+
+  use(): void {
+    console.log(`${this.name} tulistab laserkiirega! Kahju: ${this.damage}, Ulatus: ${this.range}m`);
+  }
+}
+
+class Pistol implements Weapon {
+  name: string;
+  damage: number;
+  accuracy: number;
+
+  constructor(name: string, damage: number, accuracy: number) {
+    this.name = name;
+    this.damage = damage;
+    this.accuracy = accuracy;
+  }
+
+  use(): void {
+    console.log(`${this.name} laseb! Kahju: ${this.damage}, Täpsus: ${this.accuracy}%`);
+  }
+}
+
+const inventory: Weapon[] = [
+  new Sword("KV Multitool", 25),
+  new LaserGun("Punane Laser", 40, 300),
+  new Pistol("Glock", 15, 150),
+];
+
+console.log("Mängija relvad:");
+inventory.forEach(weapon => {
+  console.log(`- ${weapon.name} ${weapon.damage} `);
+  weapon.use();
+});
